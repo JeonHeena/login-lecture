6cd45d807702d1135cb1824ac940b13626754a68
JeonHeena <heena0727@gmail.com>
2024-07-12 11:57:25 +0900

    회원가입 페이지 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -9,13 +9,15 @@ const output = {
   login: (req, res) => {
     res.render("home/login");
   },
+  register: (req, res) => {
+    res.render("home/register");
+  },
 };
 
 const process = {
   login: (req, res) => {
     const user = new User(req.body);
     const response = user.login();
-    console.log(response);
     return res.json(response);
   },
 };
