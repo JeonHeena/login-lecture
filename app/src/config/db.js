ebc0bace0466b6032826053431a5cc0a0678fbec
JeonHeena <heena0727@gmail.com>
2024-07-16 17:44:17 +0900

    winston 프로젝트에 적용

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,10 +1,11 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "",
-  user: "",
-  password: "modernheena",
-  database: "login_lecture",
+  host: process.env.DB_HOST,
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 db.connect();
